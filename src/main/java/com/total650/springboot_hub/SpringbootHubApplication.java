f59b585babc874ada8deaeeaadc1be37a3449d67
phamanhduc <zyzz15620@gmail.com>
2024-12-15 04:18:29 +0700

    [ID-01]: Project init

---
+++ src/main/java/com/total650/springboot_hub/SpringbootHubApplication.java
@@ -0,0 +1,13 @@
+package com.total650.springboot_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/total650/springboot_hub/SpringbootHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.total650.springboot_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
