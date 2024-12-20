6e683670346e8108d7a2c44bcaf50077d528a8ba
phamanhduc <zyzz15620@gmail.com>
2024-12-21 00:16:59 +0700

    [ID-23]: Impl: securing REST APIs with Database Authentication, turning off in-memory authentication

--- src/main/java/com/total650/springboot_hub/config/SecurityConfig.java
+++ src/main/java/com/total650/springboot_hub/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.total650.springboot_hub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +20,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity //This is to enabled internal annotations like @PreAuthorize as we're using in PostController
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+        //This method will automatically get userDetailsService and passwordEncoder() to do database-authentication
     }
 
     @Bean
@@ -36,26 +50,26 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        //password doesn't receive normal string
-        UserDetails user = User.builder()
-                .username("duc")
-                .password(passwordEncoder().encode("duc"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-
-        ///Either we have this class to create user or set up in application.properties, like this:
-        //spring.security.user.name=test
-        //spring.security.user.password=test
-        //spring.security.user.roles=ADMIN
-        ///But this class is better to manage multiple account, and also to control level of security for each type of account
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        //password doesn't receive normal string
+//        UserDetails user = User.builder()
+//                .username("duc")
+//                .password(passwordEncoder().encode("duc"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//
+//        ///Either we have this class to create user or set up in application.properties, like this:
+//        //spring.security.user.name=test
+//        //spring.security.user.password=test
+//        //spring.security.user.roles=ADMIN
+//        ///But this class is better to manage multiple account, and also to control level of security for each type of account
+//    }
 }
--- src/main/java/com/total650/springboot_hub/security/CustomUserDetailsService.java
+++ src/main/java/com/total650/springboot_hub/security/CustomUserDetailsService.java
@@ -18,11 +18,14 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private AccountRepository accountRepository;
 
+    /// Autowired annotation is not needed because if a @Service class only has a single parameterized constructor, spring will automatically inject the dependency
+    /// Autowired is needed when the constructor has more than 1 params like CommentService
     @Autowired
     public CustomUserDetailsService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
 
+    /// This method I just follow the instruction
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
         Account account = accountRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
@@ -35,4 +38,14 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(account.getEmail(), account.getPassword(), authorities);
     }
+
+    ///WHY DO WE NEED THIS INTERFACE and loadUserByUsername() ???
+    //When a request with Basic Authentication headers is sent
+    //The request first come to AuthenticationFilter, AuthenticationFilter will return an Authentication Object (containing username and password)
+    //The Authentication Object will be passed to AuthenticationManager, this has an authenticate() method.
+    //Atm the AuthenticationManager don't know which authentication Provider it has to call because there are 3 different AuthenticationProviders (OAuth2, LDAP, DAO)
+    //Each AuthenticationProvider has support() and authenticate() method so that AuthenticationManager can internally call the method to select which provider
+    //However all AuthenticationProvider need to uses loadByUsername() from CustomUserDetailsService
+    //This is how Database Authentication works
+
 }
